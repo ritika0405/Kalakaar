36e793b7136ee0966d6ae48c908ae1788486d876
Ritika <riya.ritika1112@gmail.com>
2024-01-06 22:01:59 +0530

    updated backend endpoint

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors())
-app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
